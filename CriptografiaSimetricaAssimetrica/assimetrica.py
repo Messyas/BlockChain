339b7b930d31a2854635da99606add59cfe5b08c
Messyas <messyasgoisfranca@gmail.com>
2025-06-29 20:07:45 -0400

    cifrador de rsa

--- CriptografiaSimetricaAssimetrica/assimetrica.py
+++ CriptografiaSimetricaAssimetrica/assimetrica.py
@@ -7,3 +7,10 @@ def gerrar_chave():
     chave_privada = chave.export_key()
     chave_publica = chave.publickey().export_key()
     return chave_privada, chave_publica
+
+def cifrar(texto, chave_publica):
+    """Cifra um texto usando a chave pública RSA."""
+    rsa_chave_publica = RSA.import_key(chave_publica)
+    cipher = PKCS1_OAEP.new(rsa_chave_publica)
+    texto_cifrado = cipher.encrypt(texto.encode())
+    return texto_cifrado
